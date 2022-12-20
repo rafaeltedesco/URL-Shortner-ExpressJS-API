ff561b5c85aec8befe4e10afa75d02831078f5ed
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 13:31:54 -0300

    test: refactor create short-url POST route

--- tests/integration/shortner.test.js
+++ tests/integration/shortner.test.js
@@ -9,7 +9,7 @@ const { expect } = chai
 describe('Test Shortner funcionality', function () {
     const testConfig = {
         testURL: '/short-url',
-        incomingURL: 'https://www.google.com',
+        incomingURL: { url: 'https://www.google.com'},
         expectedStatus: 200,
         expectedBody: {
             id: 1,
@@ -21,9 +21,7 @@ describe('Test Shortner funcionality', function () {
         it('should produce a short version of an incoming url', async function () {
             const response = await chai.request(app)
                 .post(testConfig.testURL)
-                .send({
-                    url: testConfig.incomingURL
-                })
+                .send(testConfig.incomingURL)
 
             expect(response).to.have.status(testConfig.expectedStatus)
             expect(response.body).to.deep.equal(testConfig.expectedBody)

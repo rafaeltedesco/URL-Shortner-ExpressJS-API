026e0094518e2288fa9c4145c90d76308366785f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 18:30:02 -0300

    test: should return all user's urls

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -1,8 +1,8 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../../src/app");
-const sinon = require('sinon')
-const jwt = require('jsonwebtoken')
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
 
 chai.use(chaiHttp);
 
@@ -18,32 +18,69 @@ describe("Test Users Route", function () {
           password: 1234,
         },
         expected: {
-          status: 200
+          status: 200,
         },
         header: {
-            'Authorization': 'Bearer abcd'
-        }
+          Authorization: "Bearer abcd",
+        },
       };
       it("should return a token when user login with valid credentials", async function () {
         const userData = {
-            id: 1,
-            name: "Rafael",
-            email: "rafael@mail.com",
-            iat: 1671567149,
-          }
-        sinon.stub(jwt, 'verify').returns(userData)
+          id: 1,
+          name: "Rafael",
+          email: "rafael@mail.com",
+          iat: 1671567149,
+        };
+        sinon.stub(jwt, "verify").returns(userData);
         const response = await chai
           .request(app)
           .post(successTestConfig.testUrl)
           .set(successTestConfig.header)
           .send(successTestConfig.loginData);
         expect(response).to.have.status(successTestConfig.expected.status);
-        expect(response.body).to.haveOwnProperty('token')
-        expect(response.body.token).to.have.length.greaterThan(3)
+        expect(response.body).to.haveOwnProperty("token");
+        expect(response.body.token).to.have.length.greaterThan(3);
       });
     });
   });
 
+  describe("GET /urls", function () {
+    describe("success case", function () {
+      const successTestConfig = {
+        testUrl: "/urls",
+        expected: {
+          status: 200,
+        },
+        header: {
+          Authorization: "Bearer abcd",
+        },
+      };
+      it.skip("should show all user's urls", async function () {
+        const expectedUrls = [
+          {
+            id: 7,
+            shortnedUrl: "http://localhost:3000/1234",
+            originalUrl: "http://www.google.com/test",
+            createdAt: "2022-12-05",
+          },
+          {
+            id: 4,
+            shortnedUrl: "http://localhost:3000/1234",
+            originalUrl: "http://www.google.com/test",
+            createdAt: "2022-10-05",
+          },
+        ];
+       
+        const response = await chai
+          .request(app)
+          .get(successTestConfig.testUrl)
+          .set(successTestConfig.header);
+
+        expect(response).to.have.status(200);
+        expect(response.body).to.deep.equal(expectedUrls);
+      });
+    });
+  });
   describe("GET /profile", function () {
     describe("success case", function () {
       it.skip("should return user data", async function () {});

1f616cd27f1f6cedaa3f5ca6e9b28a9c29466570
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 13:40:12 -0300

    feat: should return 400 and message url field not found when req.body does not contain "url"

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Welcome"
@@ -9,6 +11,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/short-url', (req, res)=> {
+    const { url } = req.body
+    if (!url) {
+        return res.status(400).json({
+            message: '"url" field not found'
+        })
+    }
     res.status(200).json({
         id: 1,
         shortned_url: 'http://localhost:3000/1234',

be8acd97ea51b32900dea64266e277adce3f86f4
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 17:50:52 -0300

    feat: return headerValueIsNotValid

--- src/middlewares/auth/authUser.js
+++ src/middlewares/auth/authUser.js
@@ -1,13 +1,19 @@
 const { decodeTokenJWT } = require("../../services/jwt/tokenValidator");
-const { headerKeyNotProvided } = require("../../utils/errors/errorsResponses");
+const { headerKeyNotProvided, headerValueIsNotValid } = require("../../utils/errors/errorsResponses");
 
 const isAuthenticated = async (req, _res, next) => {
   const { authorization } = req.headers;
   if (!authorization) return next(headerKeyNotProvided('token'))
   const token = authorization.split(" ")[1];
-  const userData = await decodeTokenJWT(token);
+  try {
+    const userData = await decodeTokenJWT(token);
   req.user = userData;
   next();
+  }
+  catch(err) {
+    next(headerValueIsNotValid('token'))
+  }
+  
 };
 
 module.exports = {

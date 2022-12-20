6e62bc8c2faa9045e08121864bc94caa15e8cbb8
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 17:41:59 -0300

    feat: return headerKeyError if authorization is not provide

--- src/middlewares/auth/authUser.js
+++ src/middlewares/auth/authUser.js
@@ -1,7 +1,9 @@
 const { decodeTokenJWT } = require("../../services/jwt/tokenValidator");
+const { headerKeyNotProvided } = require("../../utils/errors/errorsResponses");
 
 const isAuthenticated = async (req, _res, next) => {
   const { authorization } = req.headers;
+  if (!authorization) return next(headerKeyNotProvided('token'))
   const token = authorization.split(" ")[1];
   const userData = await decodeTokenJWT(token);
   req.user = userData;

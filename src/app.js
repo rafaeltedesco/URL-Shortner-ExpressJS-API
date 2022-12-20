c82058a1dd89fb911fbde5fac22c883de556e39a
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 13:29:35 -0300

    feat: create short-url POST route

--- src/app.js
+++ src/app.js
@@ -8,4 +8,12 @@ app.get('/', (req, res) => {
     })
 })
 
+app.post('/short-url', (req, res)=> {
+    res.status(200).json({
+        id: 1,
+        shortned_url: 'http://localhost:3000/1234',
+        original_url: 'https://www.google.com'
+    })
+})
+
 module.exports = app;

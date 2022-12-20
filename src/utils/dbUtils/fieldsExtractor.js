b202f202aaad13a98fede2865ffa3723dea9905b
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 16:10:53 -0300

    feat: add snakeize to parse content

--- src/utils/dbUtils/fieldsExtractor.js
+++ src/utils/dbUtils/fieldsExtractor.js
@@ -1,9 +1,11 @@
+const snakeize = require('snakeize')
 
 const joinFields = (content, separator=',') => content.join(`${separator} `)
 
 const parseContentToSQL = (content) => {
-    const fields = Object.keys(content)
-    const values = Object.values(content)
+    const snakeizedContent = snakeize(content)
+    const fields = Object.keys(snakeizedContent)
+    const values = Object.values(snakeizedContent)
     const placeholders = Array(fields.length).fill('?')
     return {
         fields: joinFields(fields),

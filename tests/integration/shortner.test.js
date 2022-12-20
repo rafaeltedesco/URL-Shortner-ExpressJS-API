94713bd41ec0c8a73ff88a6a72b6f370e625722f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-20 14:21:20 -0300

    refactor: change error message where it return 422

--- tests/integration/shortner.test.js
+++ tests/integration/shortner.test.js
@@ -44,20 +44,19 @@ describe('Test Shortner funcionality', function () {
                 expect(response).to.have.status(failureTestConfig.expectedStatus)
                 expect(response.body).to.deep.equal(failureTestConfig.expectedBody)
             })
-            it('should return status 422 when request contains a url but it\'s invalid', async function () {
+            it('should return status 422 when request contains a url but invalid', async function () {
                 failureTestConfig.incomingURL = {
                     url: 'abcde'
                 }
                 failureTestConfig.expectedStatus = 422
                 failureTestConfig.expectedBody = {
-                    message: 'Invalid "url"'
+                    message: '"url" invalid'
                 }
                 const response = await chai.request(app)
                     .post(failureTestConfig.testURL)
                     .send(failureTestConfig.incomingURL)
                 expect(response).to.have.status(failureTestConfig.expectedStatus)
                 expect(response.body).to.deep.equal(failureTestConfig.expectedBody)
-                
             })
         })
     })

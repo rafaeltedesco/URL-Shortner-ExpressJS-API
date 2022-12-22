fea551da8cfbf1312a368fcfa4aa5f56bfbfab3f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-22 09:50:28 -0300

    feat: turn email field into Unique

--- src/database/dumps/db.sql
+++ src/database/dumps/db.sql
@@ -5,7 +5,7 @@ USE shortner;
 CREATE TABLE users(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
